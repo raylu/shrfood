ea451d7b009ce7e6ab05a6c4a7e83a5dd6a8425d
raylu <90059+raylu@users.noreply.github.com>
2021-09-22 17:59:44 -0700

    discord links, multi-image tweets

--- www/script.js
+++ www/script.js
@@ -4,16 +4,26 @@
 	const res = await fetch('food_tweets.json');
 	const data = await res.json();
 
-	const section = document.querySelector('section');
+	const main = document.querySelector('main');
 	for (const tweet of data) {
 		const article = document.createElement('article');
-		article.innerText = tweet.text;
+
+		const link = document.createElement('a');
+		link.innerText = tweet['text'];
+		link.href = tweet['discord_link'];
+		article.append(link);
+
+		const div = document.createElement('div');
+		if (tweet['images'].length > 1)
+			div.classList.add('multi');
 		for (const media_id of tweet['images']) {
 			const img = document.createElement('img');
 			img.src = `img/${media_id}.webp`;
 			img.loading = 'lazy';
-			article.append(img);
+			div.append(img);
 		}
-		section.append(article);
+		article.append(div);
+
+		main.append(article);
 	}
 })();
